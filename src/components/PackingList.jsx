23eb9697dd2a40c57ffd9c754c2b92661558e8f7
Julyemerson Leonizio <jb.leonizio@gmail.com>
2024-06-06 00:19:07 -0300

    first commit

---
+++ src/components/PackingList.jsx
@@ -0,0 +1,14 @@
+import { initialItems } from "../data/data";
+import Item from "./Item";
+
+export default function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
